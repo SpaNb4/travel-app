5a1962bb31157a3416da896ba38042cc3044870e
magklax <nadic2006@yandex.ru>
2021-03-06 12:49:27 +0100

    refactor: rename component

--- src/components/MainGrid/index.js
+++ src/components/MainGrid/index.js
@@ -1,31 +1,3 @@
-import React from 'react';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import SimpleCard from './../SimpleCard';
-
-const GridItem = () => {
-	return (
-		<Grid item xs={12} sm={6}>
-			<SimpleCard />
-		</Grid>
-	);
-};
-
-const MainGrid = () => {
-	return (
-		<Container>
-			<Grid container spacing={1}>
-				<GridItem />
-				<GridItem />
-				<GridItem />
-				<GridItem />
-				<GridItem />
-				<GridItem />
-				<GridItem />
-				<GridItem />
-			</Grid>
-		</Container>
-	);
-};
+import MainGrid from './MainGrid';
 
 export default MainGrid;
