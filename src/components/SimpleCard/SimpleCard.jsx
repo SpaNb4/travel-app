c34b57d861f93bfaf3cf370e868e0f2fc11e6c01
kovalenkoiryna15 <52598991+kovalenkoiryna15@users.noreply.github.com>
2021-03-08 00:46:05 +0300

    fix: remove styles from code

--- src/components/SimpleCard/SimpleCard.jsx
+++ src/components/SimpleCard/SimpleCard.jsx
@@ -1,38 +1,20 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import './SimpleCard.scss'
 
-const useStyles = makeStyles({
-	root: {},
-	bullet: {
-		display: 'inline-block',
-		margin: '0 2px',
-		transform: 'scale(.8)',
-	},
-	title: {
-		fontSize: 14,
-	},
-	pos: {
-		marginBottom: 12,
-	},
-});
-
 const SimpleCard = ({ country }) => {
-	const classes = useStyles();
-
 	return (
-		<Card className={classes.root}>
-			<CardContent>
+		<Card className="simple-card">
+			<CardContent className="simple-card__content">
 				{country.countryName.en}
-			</CardContent>
-			<CardActions>
-				<Button size="small">Learn More</Button>
-			</CardActions>
+				<CardActions>
+					<Button size="small">Learn More</Button>
+				</CardActions>
+			</CardContent>			
 		</Card>
 	);
 };
