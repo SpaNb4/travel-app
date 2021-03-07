1d1c8b09847bf49e09b752c892b3f60b8bafb682
kovalenkoiryna15 <52598991+kovalenkoiryna15@users.noreply.github.com>
2021-03-07 23:53:44 +0300

    feat: countries store

--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,18 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import { combineReducers } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import countriesReducer from './countries/reducer';
 
-const rootReducer = combineReducers({ countriesReducer });
+const rootReducer = combineReducers({
+	countries: countriesReducer,
+});
 
 const composeEnhancers =
 	typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
 		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
 		: compose;
 
-const enhancer = composeEnhancers(applyMiddleware());
+const enhancer = composeEnhancers(applyMiddleware(thunkMiddleware));
 
 const store = createStore(rootReducer, enhancer);
 
