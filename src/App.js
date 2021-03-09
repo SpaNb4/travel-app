abc89628f52dbe1d21dc62a28017cf17d75707ed
kovalenkoiryna15 <52598991+kovalenkoiryna15@users.noreply.github.com>
2021-03-09 19:02:30 +0300

    feat: add ScrollToTop component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import classes from './App.module.scss';
 import Header from './components/Header';
 import Footer from './components/Footer';
+
 import Home from './containers/home/';
 import Country from './containers/country/';
 
@@ -22,14 +23,12 @@ function App() {
 	return (
 		<div className={classes.App}>
 			<Header />
-			<Router>
-				<Switch>
-					<Route exact path="/">
-						<Home />
-					</Route>
-					<Route exact path="/country/:id" component={Country} />
-				</Switch>
-			</Router>
+			<Switch>
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route exact path="/country/:id" component={Country} />
+			</Switch>
 			<Footer />
 		</div>
 	);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import './i18n';
 import store from './store/store';
 import './index.scss';
@@ -9,7 +11,10 @@ import App from './App';
 ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			<Router>
+				<ScrollToTop />
+				<App />
+			</Router>
 		</Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
