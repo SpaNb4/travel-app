e35b9b041d9f0eb5648839ff7e71e16e672d89ab
Dmitry Yermoshkin <spanb4@gmail.com>
2021-03-12 15:33:15 +0300

    feat: add auth and register forms

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -15,12 +15,18 @@ import Search from '../Search';
 import { updateCurrLng } from '../../store/app/actions';
 import { getCurrLng } from '../../store/app/slices';
 import './Header.scss';
+import RegisterForm from './AuthForm/RegisterForm/RegisterForm';
+import LoginForm from './AuthForm/LoginForm/LoginForm';
+import { AUTH_URL } from './../../common/constants';
+import { PersonAdd } from '@material-ui/icons';
 
 const Header = () => {
 	const dispatch = useDispatch();
 	const [isLngOpen, setIsLngOpen] = useState(false);
+	const [isRegisterOpen, setIsRegisterOpen] = useState(false);
+	const [isLoginOpen, setIsLoginOpen] = useState(false);
 	const currLng = useSelector(getCurrLng);
-	const auth = true;
+	const [isAuth, setIsAuth] = useState(false);
 
 	useEffect(() => {
 		i18n.changeLanguage(currLng);
@@ -37,6 +43,38 @@ const Header = () => {
 
 	function handleLngOpen() {
 		setIsLngOpen(true);
+	}
+
+	function handleRegisterOpen() {
+		setIsRegisterOpen(true);
+	}
+
+	function handleRegisterClose() {
+		setIsRegisterOpen(false);
+	}
+
+	function handleLoginOpen() {
+		setIsLoginOpen(true);
+	}
+
+	function handleLoginClose() {
+		setIsLoginOpen(false);
+	}
+
+	function handleLogoutClick() {
+		fetch(`${AUTH_URL}/logout`, {
+			method: 'get',
+			headers: { 'Content-Type': 'application/json' },
+			credentials: 'include',
+		})
+			.then((res) => res.json())
+			.then((res) => {
+				if (res) {
+					setIsAuth(false);
+					localStorage.removeItem('username');
+					localStorage.removeItem('avatar');
+				}
+			});
 	}
 
 	return (
@@ -65,20 +103,35 @@ const Header = () => {
 							<MenuItem value="de">DE</MenuItem>
 							<MenuItem value="ru">RU</MenuItem>
 						</Select>
-
-						{auth && (
+						<div>
 							<IconButton
-								aria-label="account of current user"
+								onClick={handleRegisterOpen}
+								aria-controls="menu-appbar"
+								aria-haspopup="true"
+								color="inherit"
+							>
+								<PersonAdd />
+							</IconButton>
+							<IconButton
+								onClick={handleLoginOpen}
 								aria-controls="menu-appbar"
 								aria-haspopup="true"
 								color="inherit"
 							>
 								<AccountCircleIcon />
 							</IconButton>
-						)}
+						</div>
 					</div>
 				</Toolbar>
 			</Container>
+			<RegisterForm isOpen={isRegisterOpen} handleClose={handleRegisterClose} />
+			<LoginForm
+				isOpen={isLoginOpen}
+				handleClose={handleLoginClose}
+				setIsAuth={setIsAuth}
+				isAuth={isAuth}
+				handleLogoutClick={handleLogoutClick}
+			/>
 		</AppBar>
 	);
 };
