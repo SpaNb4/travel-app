36d1deb823969111b82f1ab0bf38d8de1481dcd2
Dmitry Yermoshkin <spanb4@gmail.com>
2021-03-13 22:21:47 +0300

    fix: get avatar

--- src/common/constants.js
+++ src/common/constants.js
@@ -3,7 +3,7 @@ export const DEFAULT_LANGUAGE = 'en';
 export const LANG_STORAGE_KEY = 'lng';
 
 // URLs
-export const AUTH_URL = 'https://spanb4-travel-app.herokuapp.com';
+export const AUTH_URL = 'http://spanb4-travel-app.herokuapp.com';
 export const COUNTRIES_URL = 'https://rs-travel-backend.herokuapp.com/countries';
 export const PLACES_URL = 'https://rs-travel-backend.herokuapp.com/places';
 
