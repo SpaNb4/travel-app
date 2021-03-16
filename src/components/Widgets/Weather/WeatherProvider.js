8991e4eba79e8e8d34cbc615638adcbbedb34d96
magklax <nadic2006@yandex.ru>
2021-03-16 11:26:56 +0100

    fix: change weather url

--- src/components/Widgets/Weather/WeatherProvider.js
+++ src/components/Widgets/Weather/WeatherProvider.js
@@ -1,6 +1,6 @@
 const apiKey = 'b1b35bba8b434a28a0be2a3e1071ae5b';
 const baseURL = 'https://api.openweathermap.org/data/2.5/weather';
-const assetsBaseURL = 'http://openweathermap.org/img/w';
+const assetsBaseURL = 'https://openweathermap.org/img/w';
 const iconExtension = 'png';
 const units = 'metric';
 
