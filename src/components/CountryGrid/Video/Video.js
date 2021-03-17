c733a1df12e15f1a9a0a3c126c2d9ce7c1fe012a
Dmitry Yermoshkin <spanb4@gmail.com>
2021-03-17 22:50:36 +0300

    fix: video width

--- src/components/CountryGrid/Video/Video.js
+++ src/components/CountryGrid/Video/Video.js
@@ -3,7 +3,7 @@ import ReactPlayer from 'react-player/lazy';
 import { PropTypes } from 'prop-types';
 
 export default function Video({ videoUrl }) {
-	return <ReactPlayer url={videoUrl} controls={true} />;
+	return <ReactPlayer url={videoUrl} controls={true} width="768px" />;
 }
 Video.propTypes = {
 	videoUrl: PropTypes.string.isRequired,
